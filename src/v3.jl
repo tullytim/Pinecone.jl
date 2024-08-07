b2efa81a0fd20fcfced9d1dcd5c27774e3b10b19
vdayanand <venkatesh@juliacomputing.com>
2024-08-07 14:15:42 +0530

    add basic docstrings

--- src/v3.jl
+++ src/v3.jl
@@ -4,12 +4,12 @@ struct PineconeContextv3
     apikey::String
 end
 Base.show(io::IO, ctx::PineconeContextv3) = print(io, ctx.apikey)
-struct Serverless 
+struct Serverless
     cloud::String
     region::String
 end
 
-@kwdef struct PodSpec 
+@kwdef struct PodSpec
     environment::String
     podtype::String
     ## TBD
@@ -46,6 +46,20 @@ pineconeMakeURLForIndex() = begin
     "https://api.pinecone.io/indexes"
 end
 
+# note no repsonse body from create, derive success from the HTTP 204
+"""
+   create_index(ctx::PineconeContext, indexname::String, dimension::Int64; metric::String="cosine", pods::Int64=1, replicas::Int64=1, shards::Int64=1, podtype::String="p1")
+
+Creates an index with a given PineconeContext, which can be accessed by a call to init(), the name of the index, and the number of dimensions.
+
+This function returns a JSON blob as a string, or nothing if it failed. Do recommend using JSON3 to parse the blob.
+
+# Example
+```julia
+context = Pinecone.init("abcd-123456-zyx", "us-west1-gcp")
+result = Pinecone.create_index(context, testindexname, 10, metric="euclidean", replicas=2, shards=1)
+```
+"""
 function create_index(ctx::PineconeContextv3, indexname::String, dimension::Int, metric::String="cosine", cloud::String="aws", region::String="us-east-1")
     dimension = dimension
     if(dimension > MAX_DIMS)
@@ -69,6 +83,22 @@ function create_index(ctx::PineconeContextv3, indexname::String, dimension::Int,
     response = pineconeHTTPPost(url, ctx.apikey, JSON3.write(body))
 end
 
+"""
+    init(apikey::String, environment::String)
+
+Initialize the Pinecone environment using your API Key and a specific environment.
+
+This returns a PineconeContext instance which you'll use for subsequent calls such as query() and upsert().
+Your API Key and the cloud environment for your indexes are easily found in the Pinecone console.
+On failure returns nothing.
+
+# Example
+```julia
+using Pinecone
+context = Pinecone.init("abcd-123456-zyx", "us-west1-gcp")
+```
+"""
+
 function init_v3(apikey::String)
     return PineconeContextv3(apikey)
 end
@@ -81,7 +111,33 @@ function list_index_objects(ctx::PineconeContextv3)
     JSON3.read(JSON3.write(indexes), Vector{PineconeIndexv3})
 end
 
+
+"""
+    list_indexes(context::PineconeContext)
+
+Returns a JSON array listing indexes for a given account, which is indicated by the PineconeContext instance passed in.
+
+# Example
+```julia-repl
+julia> context = Pinecone.init("asdf-1234-zyxv", "us-west1-gcp")
+Pinecone.list_indexes(context)
+["example-index", "filter-example"]
+```
+"""
 list_indexes(ctx::PineconeContextv3) = map(x->x.name, list_index_objects(ctx))
+
+
+"""
+    Index(indexname::String)
+
+Returns a PineconeIndex type which is used for query() and upsert() of data against a specific index.
+
+# Example
+```julia-repl
+julia> Pinecone.Index("my-index")
+PineconeIndex connected to my-index
+```
+"""
 
 function Index(ctx::PineconeContextv3, indexname::String)
     url = pineconeMakeURLForIndex(indexname)
@@ -95,11 +151,38 @@ function describe_index_stats(ctx::PineconeContextv3, indexobj::PineconeIndexv3)
     return String(response.body)
 end
 
+"""
+    delete_index(ctx::PineconeContext, indexobj::PineconeIndex)
+
+Deletes an index, returns true on successful response from Pinecone backend.
+
+This delets a given Pinecone index, note that this is an asynch call and doesn't guarantee that on return that the index is actually deleted (yet).
+
+# Example
+```julia
+Pinecone.delete_index(context, Pinecone.Index("index-to-delete"))
+```
+"""
 function delete_index(ctx::PineconeContextv3, indexname::String)
     url = pineconeMakeURLForIndex(indexname)
     response = pineconeHTTPDelete(url, ctx.apikey)
 end
 
+"""
+    upsert(ctx::PineconeContext, indexobj::PineconeIndex, vectors::Vector{PineconeVector}, namespace::String="")
+
+upserts a Julia Vector of type PineconeVector using the given PineconeContext and PineconeIndex with an optional namespace (Defaults to not being applied to query if not passed.)
+On success returns a JSON blob as a String type, and nothing if it fails.
+This function returns a JSON blob as a string, or nothing if it failed. Do recommend using JSON3 to parse the blob.
+
+# Example
+```julia-repl
+julia> testvector = Pinecone.PineconeVector("testid", [0.3,0.11,0.3,0.3,0.3,0.3,0.3,0.3,0.4,0.3])
+context = Pinecone.init("abcd-123456-zyx", "us-west1-gcp")
+index = PineconeIndex("myindex")
+result = Pinecone.upsert(context, index, [testvector], "testnamespace")
+```
+"""
 function upsert(ctx::PineconeContextv3, indexobj::PineconeIndexv3, vectors::Vector{PineconeVector}, namespace::String="")
     if(length(vectors) > MAX_UPSERT_VECTORS)
         throw(ArgumentError("Max number of vectors per upsert is " * string(MAX_UPSERT_VECTORS)))
@@ -114,6 +197,20 @@ function upsert(ctx::PineconeContextv3, indexobj::PineconeIndexv3, vectors::Vect
     return String(response.body)
 end #upsert
 
+"""
+    delete(ctx::PineconeContext, indexobj::PineconeIndex, ids::Array{String}, deleteall::Bool, namespace::String)
+
+Deletes vectors indexed by ``ids``.  **Warning:** The entire namespace for the vector will be deleted if deleteall=true
+
+Returns JSON blob as ``String`` of empty object or nothing if fails.
+# Example
+```julia-repl
+julia> context = Pinecone.init("asdf-1234-zyxv", "us-west1-gcp")
+index = PineconeIndex("my-index")
+Pinecone.delete(context, index, ["deleteme1", "deleteme2"], false, "timnamespace")
+{}
+```
+"""
 function delete(ctx::PineconeContextv3, indexobj::PineconeIndexv3, ids::Array{String}, deleteall::Bool=false, namespace::String="")
     if(length(ids) > MAX_DELETE)
         throw(ArgumentError("Max number of vectors per delete is " * string(MAX_DELETE)))
@@ -140,7 +237,7 @@ function query(ctx::PineconeContextv3, indexobj::PineconeIndexv3, queries::Pinec
     query(ctx, indexobj, queries.values, topk, namespace, includevalues, includemeta, filter)
 end
 
-function query(ctx::PineconeContextv3, indexobj::PineconeIndexv3, vector::Vector{T}, topk::Int64=10, namespace::String="", includevalues::Bool=false, 
+function query(ctx::PineconeContextv3, indexobj::PineconeIndexv3, vector::Vector{T}, topk::Int64=10, namespace::String="", includevalues::Bool=false,
         includemeta::Bool=false, filter::Dict{String, Any}=Dict{String,Any}()) where {T<:AbstractFloat}
     if topk > MAX_TOPK
         throw(ArgumentError("topk larger than largest topk available of " * string(MAX_TOPK)))
@@ -155,7 +252,7 @@ function query(ctx::PineconeContextv3, indexobj::PineconeIndexv3, vector::Vector
     body = Dict{String, Any}("topK"=>topk, "vector"=>vector, "includeValues"=>includevalues, "includeMetadata"=>includemeta, "namespace"=>namespace)
     if(length(filter) > 0)
         body["filter"] = filter;
-    end    
+    end
     postbody = JSON3.write(body)
     response = pineconeHTTPPost(url, ctx.apikey, postbody)
     return String(response.body)
