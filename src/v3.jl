26a99c102b231d01217d47b002bbc3a8c52c017d
vdayanand <venkatesh@juliacomputing.com>
2024-08-07 14:36:49 +0530

    up docs add remaining structure

--- src/v3.jl
+++ src/v3.jl
@@ -220,6 +220,21 @@ function delete(ctx::PineconeContextv3, indexobj::PineconeIndexv3, ids::Array{St
     return String(response.body)
 end
 
+"""
+    fetch(ctx::PineconeContext, indexobj::PineconeIndex, ids::Array{String}, namespace::String)
+
+Fetches vectors based on the vector ids for each vector, provided as the ``ids`` array for a given namespace.  .
+Note that namespace is optional, and if not provided defaults to not using it to filter query results by namespace.
+Returns JSON blob as ``String`` show below, or ``nothing`` on failure.
+# Example
+```julia-repl
+julia> context = Pinecone.init("asdf-1234-zyxv", "us-west1-gcp")
+index = PineconeIndex("my-index")
+Pinecone.fetch(context, index, ["testid", "testid2"], "testnamespace")
+{"vectors":{"testid":{"id":"testid","values":[0.3,0.11,0.3,0.3,0.3,0.3,0.3,0.3,0.4,0.3],"metadata":{"year":2019,"genre":"documentary"}},
+"testid2":{"id":"testid2","values":[0.3,0.11,0.3,0.3,0.3,0.3,0.3,0.3,0.4,0.3],"metadata":{"genre":"documentary","year":2019}}},"namespace":"testnamespace"}
+```
+"""
 function fetch(ctx::PineconeContextv3, indexobj::PineconeIndexv3, ids::Array{String}, namespace::String="")
     if(length(ids) > MAX_FETCH)
         throw(ArgumentError("Max number of vectors per fetch is " * string(MAX_FETCH)))
@@ -231,10 +246,56 @@ function fetch(ctx::PineconeContextv3, indexobj::PineconeIndexv3, ids::Array{Str
     return String(response.body)
 end
 
+
+"""
+   query(ctx::PineconeContext, indexobj::PineconeIndex, queries::Vector{PineconeVector}, topk::Int64=10, namespace::String="", includevalues::Bool=false, includemeta::Bool=false, filter::Dict{String, Any}=Dict{String,Any}())
+
+Query an index using the given context that match ``queries`` passed in.  The ``PineconeVector`` that is queries is a simple ``PineconeVector`` as described above.
+Note there is an another version of ``query()`` that takes in a ``Vector{Vector{<:AbstractFloat}}`` for the ``queries`` parameter.  Functionally equivalent.
+Returns JSON blob as a ``String`` with the results on success, ``nothing`` on failure.
+
+topk is an optional parameter which defaults to 10 if not specified.  Do recommend using JSON3 to parse the blob.
+
+
+# Example
+```julia-repl
+julia> testdict = Dict{String, Any}("genre"=>"documentary", "year"=>2019);
+julia> pinecone_context = Pinecone.init("abcdef-1234-zyxv", "us-west1-gcp")
+abcdef-1234-zyxv / us-west1-gcp / 1c9c2f3
+julia> pinecone_index = Pinecone.Index("filter-example");
+julia> testvector = Pinecone.PineconeVector("testid", [0.3,0.11,0.3,0.3,0.3,0.3,0.3,0.3,0.4,0.3], testdict)
+PineconeVector is id: testid values: [0.3, 0.11, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.4, 0.3]meta: Dict{String, Any}("genre" => "documentary", "year" => 2019)
+julia> Pinecone.query(pinecone_context, pinecone_index, [testvector], 4)
+"{\"results\":[{\"matches\":[{\"id\":\"testid\",\"score\":3.98966023e-07,\"values\":[0.3,0.11,0.3,0.3,0.3,0.3,0.3,0.3,0.4,0.3]},{\"id\":\"C\",\"score\":0.0461001098,\"values\":[0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3]}
+],\"namespace\":\"\"}]}"
+```
+"""
 function query(ctx::PineconeContextv3, indexobj::PineconeIndexv3, queries::PineconeVector, topk::Int64=10, namespace::String="", includevalues::Bool=false, includemeta::Bool=false, filter::Dict{String, Any}=Dict{String,Any}())
     #rawvectors = [queries[i].values for i in 1:length(queries)]
     query(ctx, indexobj, queries.values, topk, namespace, includevalues, includemeta, filter)
 end
+
+"""
+   query(ctx::PineconeContext, indexobj::PineconeIndex, queries::Vector{Vector{T}}, topk::Int64=10, namespace::String="", includevalues::Bool=false, includemeta::Bool=false, filter::Dict{String, Any}=Dict{String,Any}()) where {T<:AbstractFloat}
+
+Query an index using the given context that match ``queries`` passed in. Returns JSON blob as a ``String`` with the results on success, ``nothing`` on failure.
+Note there is an alternate form for ``query()`` that takes in a ``Vector{PineconeVector}`` instead.  Functionally equivalent.
+topk is an optional parameter which defaults to 10 if not specified.  Do recommend using JSON3 to parse the blob.
+
+# Example
+```julia-repl
+julia> testdict = Dict{String, Any}("genre"=>"documentary", "year"=>2019);
+julia> pinecone_context = Pinecone.init("abcdef-1234-zyxv", "us-west1-gcp")
+abcdef-1234-zyxv / us-west1-gcp / 1c9c2f3
+julia> pinecone_index = Pinecone.Index("filter-example");
+julia> testvector = Pinecone.PineconeVector("testid", [0.3,0.11,0.3,0.3,0.3,0.3,0.3,0.3,0.4,0.3], testdict)
+PineconeVector is id: testid values: [0.3, 0.11, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.4, 0.3]meta: Dict{String, Any}("genre" => "documentary", "year" => 2019)
+julia> Pinecone.query(pinecone_context, pinecone_index,
+[[0.2, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3], [0.2, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3]], 4)
+"{\"results\":[{\"matches\":[{\"id\":\"testid\",\"score\":3.98966023e-07,\"values\":[0.3,0.11,0.3,0.3,0.3,0.3,0.3,0.3,0.4,0.3]},{\"id\":\"C\",\"score\":0.0461001098,\"values\":[0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3]}
+],\"namespace\":\"\"}]}"
+```
+"""
 
 function query(ctx::PineconeContextv3, indexobj::PineconeIndexv3, vector::Vector{T}, topk::Int64=10, namespace::String="", includevalues::Bool=false,
         includemeta::Bool=false, filter::Dict{String, Any}=Dict{String,Any}()) where {T<:AbstractFloat}
