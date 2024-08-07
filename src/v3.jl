37047097924be0c3e39f7cc75abcea508436eea2
vdayanand <venkatesh@juliacomputing.com>
2024-08-07 14:19:06 +0530

    up create_index docs

--- src/v3.jl
+++ src/v3.jl
@@ -46,18 +46,17 @@ pineconeMakeURLForIndex() = begin
     "https://api.pinecone.io/indexes"
 end
 
-# note no repsonse body from create, derive success from the HTTP 204
 """
-   create_index(ctx::PineconeContext, indexname::String, dimension::Int64; metric::String="cosine", pods::Int64=1, replicas::Int64=1, shards::Int64=1, podtype::String="p1")
+create_index(ctx::PineconeContextv3, indexname::String, dimension::Int, metric::String="cosine", cloud::String="aws", region::String="us-east-1")
 
-Creates an index with a given PineconeContext, which can be accessed by a call to init(), the name of the index, and the number of dimensions.
+Creates an index with a given PineconeContextv3, which can be accessed by a call to init(), the name of the index, and the number of dimensions.
 
 This function returns a JSON blob as a string, or nothing if it failed. Do recommend using JSON3 to parse the blob.
 
 # Example
 ```julia
-context = Pinecone.init("abcd-123456-zyx", "us-west1-gcp")
-result = Pinecone.create_index(context, testindexname, 10, metric="euclidean", replicas=2, shards=1)
+context = Pinecone.init_v3("abcd-123456-zyx")
+result =     Pinecone.create_index(ctx, "my-index", 2, "euclidean")
 ```
 """
 function create_index(ctx::PineconeContextv3, indexname::String, dimension::Int, metric::String="cosine", cloud::String="aws", region::String="us-east-1")
